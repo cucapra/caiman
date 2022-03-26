271a9c22cfed902880ff1b963fffee9b09ec8457
Oliver Daids <ojd5@cornell.edu>
2022-03-25 22:59:01 -0400

    More test pipelines

--- caiman-test/src/main.rs
+++ caiman-test/src/main.rs
@@ -21,6 +21,38 @@ impl pipelines::pipeline_with_gpu_gpu_communication::CpuFunctions for Callbacks
 	}
 }
 
+impl pipelines::pipeline_with_single_cpu_call::CpuFunctions for Callbacks
+{
+	fn do_thing_on_cpu( & self, value : i32 ) -> pipelines::pipeline_with_single_cpu_call::outputs::do_thing_on_cpu
+	{
+		return pipelines::pipeline_with_single_cpu_call::outputs::do_thing_on_cpu { field_0 : value + 1 };
+	}
+}
+
+impl pipelines::pipeline_with_gpu_cpu_communication::CpuFunctions for Callbacks
+{
+	fn do_thing_on_cpu( & self, value : i32 ) -> pipelines::pipeline_with_gpu_cpu_communication::outputs::do_thing_on_cpu
+	{
+		return pipelines::pipeline_with_gpu_cpu_communication::outputs::do_thing_on_cpu { field_0 : value + 1 };
+	}
+}
+
+impl pipelines::pipeline_with_cpu_cpu_communication::CpuFunctions for Callbacks
+{
+	fn do_thing_on_cpu( & self, value : i32 ) -> pipelines::pipeline_with_cpu_cpu_communication::outputs::do_thing_on_cpu
+	{
+		return pipelines::pipeline_with_cpu_cpu_communication::outputs::do_thing_on_cpu { field_0 : value + 1 };
+	}
+}
+
+impl pipelines::pipeline_with_single_gpu_call::CpuFunctions for Callbacks
+{
+	fn do_thing_on_cpu( & self, value : i32 ) -> pipelines::pipeline_with_single_gpu_call::outputs::do_thing_on_cpu
+	{
+		return pipelines::pipeline_with_single_gpu_call::outputs::do_thing_on_cpu { field_0 : value + 1 };
+	}
+}
+
 fn main()
 {
 }
@@ -48,5 +80,49 @@ mod tests
 		let callbacks = crate::Callbacks;
 		let result = crate::pipelines::pipeline_with_gpu_gpu_communication::run(&mut device, &mut queue, & callbacks, 1);
 		assert_eq!(3, result.field_0);
+	}
+
+	#[test]
+	fn test_3()
+	{
+		let instance = wgpu::Instance::new(wgpu::Backends::PRIMARY);
+		let adapter = futures::executor::block_on(instance.request_adapter(& wgpu::RequestAdapterOptions { power_preference : wgpu::PowerPreference::default(), compatible_surface : None, force_fallback_adapter : false })).unwrap();
+		let (mut device, mut queue) = futures::executor::block_on(adapter.request_device(& std::default::Default::default(), None)).unwrap();
+		let callbacks = crate::Callbacks;
+		let result = crate::pipelines::pipeline_with_single_cpu_call::run(&mut device, &mut queue, & callbacks, 1);
+		assert_eq!(2, result.field_0);
+	}
+
+	#[test]
+	fn test_4()
+	{
+		let instance = wgpu::Instance::new(wgpu::Backends::PRIMARY);
+		let adapter = futures::executor::block_on(instance.request_adapter(& wgpu::RequestAdapterOptions { power_preference : wgpu::PowerPreference::default(), compatible_surface : None, force_fallback_adapter : false })).unwrap();
+		let (mut device, mut queue) = futures::executor::block_on(adapter.request_device(& std::default::Default::default(), None)).unwrap();
+		let callbacks = crate::Callbacks;
+		let result = crate::pipelines::pipeline_with_gpu_cpu_communication::run(&mut device, &mut queue, & callbacks, 1);
+		assert_eq!(3, result.field_0);
+	}
+
+	#[test]
+	fn test_5()
+	{
+		let instance = wgpu::Instance::new(wgpu::Backends::PRIMARY);
+		let adapter = futures::executor::block_on(instance.request_adapter(& wgpu::RequestAdapterOptions { power_preference : wgpu::PowerPreference::default(), compatible_surface : None, force_fallback_adapter : false })).unwrap();
+		let (mut device, mut queue) = futures::executor::block_on(adapter.request_device(& std::default::Default::default(), None)).unwrap();
+		let callbacks = crate::Callbacks;
+		let result = crate::pipelines::pipeline_with_cpu_cpu_communication::run(&mut device, &mut queue, & callbacks, 1);
+		assert_eq!(3, result.field_0);
+	}
+
+	#[test]
+	fn test_6()
+	{
+		let instance = wgpu::Instance::new(wgpu::Backends::PRIMARY);
+		let adapter = futures::executor::block_on(instance.request_adapter(& wgpu::RequestAdapterOptions { power_preference : wgpu::PowerPreference::default(), compatible_surface : None, force_fallback_adapter : false })).unwrap();
+		let (mut device, mut queue) = futures::executor::block_on(adapter.request_device(& std::default::Default::default(), None)).unwrap();
+		let callbacks = crate::Callbacks;
+		let result = crate::pipelines::pipeline_with_single_gpu_call::run(&mut device, &mut queue, & callbacks, 1);
+		assert_eq!(2, result.field_0);
 	}
 }
