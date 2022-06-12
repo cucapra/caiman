f0f9e0498c662275808e649c2dbe5b0010309d32
Oliver Daids <ojd5@cornell.edu>
2022-06-11 21:22:31 -0400

    Switching place + slot to just slot

--- caiman-ref/src/scheduling.rs
+++ caiman-ref/src/scheduling.rs
@@ -6,13 +6,11 @@ use crate::functional;
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct BufferId(usize);
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct CommandListId(usize);
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct SubmissionId(usize);
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct FuncletInstanceId(usize);
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct SlotId(FuncletInstanceId, functional::NodeId);
+struct SlotId(usize);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct ValueId(FuncletInstanceId, functional::NodeId);
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct LogicalTimestamp(usize);
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
@@ -58,37 +56,41 @@ enum MapMode
 
 enum Binding
 {
-	LocalVariable{ value_box : Box<dyn Any> },
+	Variable{ value_box : Box<dyn Any> },
 	Buffer{ buffer_id : BufferId, start : usize, size : usize },
 	ReadOnlyMappedBuffer{ buffer_id : BufferId, start : usize, size : usize, nasty_raw_pointer : *const u8 },
 	WriteableMappedBuffer{ buffer_id : BufferId, start : usize, size : usize, nasty_raw_pointer : *mut u8 },
 }
 
+struct Slot
+{
+	// Part of the type describing the value
+	value_id_opt : Option<ValueId>,
+	// Part of the type describing when the value will be available
+	queue_place : Place,
+	queue_state : QueueState,
+	time_observed : LogicalTimestamp,
+	// Part of the type describing the resource that will hold the value
+	binding : Binding
+}
+
 // For now, it's assumed that all buffers live on the gpu (this isn't at all true) and can be mapped local (this isn't true either)
 struct Buffer
 {
-	queue_state : QueueState,
 	map_count : usize,
 	other_use_count : usize,
-	allocated_ranges : BTreeMap<usize, usize>,
 	wgpu_buffer : wgpu::Buffer
 }
 
 struct CommandEncoder
 {
-	buffer_ids : BTreeSet<BufferId>,
 	wgpu_command_encoder_opt : Option<wgpu::CommandEncoder>,
 	wgpu_command_buffers : Vec<wgpu::CommandBuffer>,
 }
 
-struct CommandList
-{
-	buffer_ids : BTreeSet<BufferId>,
-	time_submitted : LogicalTimestamp
-}
-
 struct Fence
 {
+	fenced_place : Place,
 	time_inserted_opt : Option<LogicalTimestamp>,
 	// This is disgusting in so many ways
 	completion_future : Option<Pin<Box<dyn futures::Future<Output = ()> + Send>>>
@@ -104,16 +106,21 @@ struct FuncletInstance
 
 }*/
 
+/*struct PlaceState
+{
+	logical_timestamp : LogicalTimestamp
+}*/
+
 struct SchedulerState<'device, 'queue>
 {
 	device : & 'device mut wgpu::Device,
 	queue : & 'queue mut wgpu::Queue,
 	buffers : BTreeMap<BufferId, Buffer>,
 	funclet_instances : Vec<FuncletInstance>,
-	pending_command_lists : VecDeque<CommandList>,
 	active_command_encoder_opt : Option<CommandEncoder>,
 	fences : BTreeMap<FenceId, Fence>,
-	slot_per_place_bindings : BTreeMap<SlotId, BTreeMap<Place, Binding>>,
+	slots : Vec<Option<Slot>>,
+	//place_states : BTreeMap<Place, PlaceState>,
 	local_logical_timestamp : LogicalTimestamp,
 	latest_gpu_synchronized_logical_timestamp : LogicalTimestamp,
 }
@@ -126,20 +133,35 @@ impl<'device, 'queue> SchedulerState<'device, 'queue>
 		let mut buffers = BTreeMap::<BufferId, Buffer>::new();
 		let mut fences = BTreeMap::<FenceId, Fence>::new();
 		let funclet_instances = Vec::<FuncletInstance>::new();
-		let slot_per_place_bindings = BTreeMap::<SlotId, BTreeMap<Place, Binding>>::new();
-		let pending_command_lists = VecDeque::<CommandList>::new();
-		Self{device, queue, buffers, funclet_instances, active_command_encoder_opt : None, pending_command_lists, /*submissions,*/ fences, slot_per_place_bindings, local_logical_timestamp : LogicalTimestamp::new(), latest_gpu_synchronized_logical_timestamp : LogicalTimestamp::new()}
+		let slots = Vec::<Option<Slot>>::new();
+		Self{device, queue, buffers, funclet_instances, active_command_encoder_opt : None, /*submissions,*/ fences, slots, local_logical_timestamp : LogicalTimestamp::new(), latest_gpu_synchronized_logical_timestamp : LogicalTimestamp::new()}
 	}
 
-	fn queue_state_of_binding(&self, binding : & Binding) -> QueueState
+	fn create_slot(&mut self, slot : Slot) -> SlotId
 	{
-		match binding
-		{
-			Binding::LocalVariable {value_box} => panic!("Not implemented"),
-			Binding::Buffer{buffer_id, start, size} => self.buffers[buffer_id].queue_state,
-			Binding::ReadOnlyMappedBuffer{ buffer_id, start, size, nasty_raw_pointer } => self.buffers[buffer_id].queue_state,
-			Binding::WriteableMappedBuffer{ buffer_id, start, size, nasty_raw_pointer } => self.buffers[buffer_id].queue_state,
-		}
+		let slot_id = SlotId(self.slots.len());
+		self.slots.push(Some(slot));
+		slot_id
+	}
+
+	fn destroy_slot(&mut self, slot_id : SlotId)
+	{
+		self.slots[slot_id.0] = None;
+	}
+
+	fn has_slot(&self, slot_id : SlotId) -> bool
+	{
+		self.slots[slot_id.0].is_some()
+	}
+
+	fn get_slot(&self, slot_id : SlotId) -> &Slot
+	{
+		self.slots[slot_id.0].as_ref().unwrap()
+	}
+
+	fn get_slot_mut(&mut self, slot_id : SlotId) -> &mut Slot
+	{
+		self.slots[slot_id.0].as_mut().unwrap()
 	}
 
 	/*fn instance_funclet(&mut self, funclet_id : functional::FuncletId) -> FuncletInstanceId
@@ -148,39 +170,39 @@ impl<'device, 'queue> SchedulerState<'device, 'queue>
 		FuncletInstanceId(self.funclet_instances.len() - 1)
 	}*/
 
-	pub fn assert(&mut self, slot_ids : &[SlotId], place_and_queue_state_pairs : &[(Place, QueueState)])
+	/*pub fn assert(&mut self, slot_ids : &[SlotId], place_and_queue_state_pairs : &[(Place, QueueState)])
 	{
-		for slot_id in slot_ids.iter()
+		for & slot_id in slot_ids.iter()
 		{
-			let per_place_bindings = & self.slot_per_place_bindings[slot_id];
 			for & (place, queue_state) in place_and_queue_state_pairs.iter()
 			{
-				assert_eq!(queue_state, self.queue_state_of_binding(& per_place_bindings[& place]));
+				assert_eq!(place, self.get_slot(slot_id).place);
+				assert_eq!(queue_state, self.get_slot(slot_id).queue_state);
 			}
 		}
+	}*/
+
+	pub fn bind_buffer(&mut self, buffer_id : BufferId, offset : usize, size : usize) -> SlotId
+	{
+		let time_observed = self.local_logical_timestamp;
+		let binding = Binding::Buffer{buffer_id, start : offset, size};
+		let slot = Slot{value_id_opt : None, queue_place : Place::Gpu, queue_state : QueueState::None, time_observed, binding};
+		self.create_slot(slot)
 	}
 
-	pub fn bind_buffer(&mut self, slot_id : SlotId, place : Place, buffer_id : BufferId, offset : usize, size : usize)
+	pub fn discard(&mut self, slot_ids : &[SlotId])
 	{
-		// For now, it only makes sense to treat buffers as if they exist on the gpu
-		assert!(! self.slot_per_place_bindings[& slot_id].contains_key(& Place::Local));
-		assert_eq!(place, Place::Gpu);
-		assert_eq!(self.buffers[& buffer_id].queue_state, QueueState::None);
-		self.slot_per_place_bindings.get_mut(& slot_id).unwrap().insert(place, Binding::Buffer{buffer_id, start : offset, size});
-	}
-
-	pub fn unbind(&mut self, slot_ids : &[SlotId], place : Place)
-	{
-		for slot_id in slot_ids.iter()
+		for & slot_id in slot_ids.iter()
 		{
-			let (buffer_id, is_a_mapping) = match self.slot_per_place_bindings[& slot_id][& place]
+			let mapped_buffer_id_opt = match self.get_slot(slot_id).binding
 			{
-				Binding::LocalVariable {ref value_box} => panic!("Not implemented"),
-				Binding::ReadOnlyMappedBuffer{buffer_id, start, size, nasty_raw_pointer} => (buffer_id, true),
-				Binding::WriteableMappedBuffer{buffer_id, start, size, nasty_raw_pointer} => (buffer_id, true),
-				Binding::Buffer {buffer_id, ..} => (buffer_id, false),
+				Binding::Variable {ref value_box} => None,
+				Binding::ReadOnlyMappedBuffer{buffer_id, start, size, nasty_raw_pointer} => Some(buffer_id),
+				Binding::WriteableMappedBuffer{buffer_id, start, size, nasty_raw_pointer} => Some(buffer_id),
+				Binding::Buffer {buffer_id, ..} => None,
 			};
-			if is_a_mapping
+
+			if let Some(buffer_id) = mapped_buffer_id_opt
 			{
 				let buffer : &mut Buffer = self.buffers.get_mut(& buffer_id).unwrap();
 				assert!(buffer.map_count > 0);
@@ -190,7 +212,8 @@ impl<'device, 'queue> SchedulerState<'device, 'queue>
 					buffer.wgpu_buffer.unmap();
 				}
 			}
-			self.slot_per_place_bindings.get_mut(& slot_id).unwrap().remove(& place);
+
+			self.destroy_slot(slot_id);
 		}
 	}
 
@@ -203,8 +226,34 @@ impl<'device, 'queue> SchedulerState<'device, 'queue>
 		assert_eq!(fenced_place, Place::Gpu);
 		let fence : &mut Fence = self.fences.get_mut(& fence_id).unwrap();
 		assert!(fence.time_inserted_opt.is_none());
+		fence.fenced_place = fenced_place;
 		fence.time_inserted_opt = Some(self.local_logical_timestamp);
 		fence.completion_future = Some(Box::pin(self.queue.on_submitted_work_done()));
+	}
+
+	fn transition_resource_states(&mut self, place : Place, from_state : QueueState, to_state : QueueState)
+	{
+		let time_observed = match place
+		{
+			Place::Local => self.local_logical_timestamp,
+			Place::Gpu => self.latest_gpu_synchronized_logical_timestamp,
+			Place::Cpu => panic!("Not yet implemented")
+		};
+
+		for (slot_id, slot_opt) in self.slots.iter_mut().enumerate()
+		{
+			if let Some(slot) = slot_opt
+			{
+				if slot.queue_place == place && time_observed >= slot.time_observed
+				{
+					if slot.queue_state == from_state
+					{
+						slot.time_observed = time_observed;
+						slot.queue_state = to_state;
+					}
+				}
+			}
+		}
 	}
 
 	// Stalls the queue of synced_place until signaled through the given fence
@@ -230,47 +279,39 @@ impl<'device, 'queue> SchedulerState<'device, 'queue>
 			self.latest_gpu_synchronized_logical_timestamp = time_inserted;
 		}
 
-		while let Some(mut command_list) = self.pending_command_lists.pop_front()
+		self.transition_resource_states(Place::Gpu, QueueState::Submitted, QueueState::Ready);
+
+		/*for (slot_id, slot_opt) in self.slots.iter_mut().enumerate()
 		{
-			if command_list.time_submitted > time_inserted
+			if let Some(slot) = slot_opt
 			{
-				self.pending_command_lists.push_front(command_list);
-				break;
-			}
-
-			for buffer_id in command_list.buffer_ids.iter()
-			{
-				let buffer : &mut Buffer = self.buffers.get_mut(& buffer_id).unwrap();
-
-				assert!(buffer.other_use_count > 0);
-				buffer.other_use_count -= 1;
-
-				buffer.queue_state = match buffer.queue_state
+				if slot.queue_place == Place::Gpu && time_inserted >= slot.time_observed
 				{
-					QueueState::None => panic!("Buffer was not properly encoded or submitted"),
-					QueueState::Encoded => panic!("Buffer was not properly submitted"),
-					QueueState::Submitted => QueueState::Ready,
-					QueueState::Ready => QueueState::Ready,
-				};
+					if slot.queue_state == QueueState::Submitted
+					{
+						slot.time_observed = self.latest_gpu_synchronized_logical_timestamp;
+						slot.queue_state = QueueState::Ready;
+					}
+				}
 			}
-		}
+		}*/
 	}
 
-	fn do_local_constant<T : 'static + Copy>(&mut self, slot_id : SlotId, value_ref : &T)
+	/*fn do_local_constant<T : 'static + Copy>(&mut self, slot_id : SlotId, value_ref : &T)
 	{
 		match self.slot_per_place_bindings[& slot_id].get(& Place::Local).as_ref()
 		{
 			None =>
 			{
 				assert!(! self.slot_per_place_bindings[& slot_id].contains_key(& Place::Gpu));
+				assert!(! self.has_slot(slot_id));
 				let value_box : Box<dyn Any> = Box::new(* value_ref);
-				self.slot_per_place_bindings.get_mut(& slot_id).unwrap().insert(Place::Local, Binding::LocalVariable{value_box});
+				let time_observed = self.logical_timestamp;
+				self.initialize_slot(slot_id, SlotId{value_id : , queue_place : Place::Local, queue_state : QueueState::Ready, time_observed, binding : Binding::Variable{value_box}});
+				self.slot_per_place_bindings.get_mut(& slot_id).unwrap().insert(Place::Local, Binding::Variable{value_box});
 			}
 			Some(& Binding::WriteableMappedBuffer{buffer_id, start : _, size, nasty_raw_pointer}) =>
 			{
-				let buffer : &mut Buffer = self.buffers.get_mut(& buffer_id).unwrap();
-				assert_eq!(buffer.map_count, 1);
-
 				assert_eq!(* size, std::mem::size_of::<T>());
 				let source_bytes : &[u8] = unsafe { std::slice::from_raw_parts( std::mem::transmute::<*const T, *const u8>(value_ref), std::mem::size_of::<T>()) };
 				let destination_bytes : &mut [u8] = unsafe { std::slice::from_raw_parts_mut::<u8>( *nasty_raw_pointer, std::mem::size_of::<T>()) };
@@ -290,14 +331,14 @@ impl<'device, 'queue> SchedulerState<'device, 'queue>
 
 		let value_ref : &T = match self.slot_per_place_bindings[& slot_id][& Place::Local]
 		{
-			Binding::LocalVariable{ref value_box} => value_box.downcast_ref::<T>().unwrap(),
+			Binding::Variable{ref value_box} => value_box.downcast_ref::<T>().unwrap(),
 			_ => panic!("Incorrect binding for slot")
 		};
 
 		assert_eq!(size, std::mem::size_of::<T>());
 		let bytes : &[u8] = unsafe { std::slice::from_raw_parts( std::mem::transmute::<*const T, *const u8>(value_ref), std::mem::size_of::<T>()) };
 		self.queue.write_buffer(& self.buffers[& buffer_id].wgpu_buffer, start.try_into().unwrap(), bytes);
-	}
+	}*/
 
 	/*fn encode_gpu_call_gpu_compute_function<F : GpuComputeFunction>(&mut self, slot_id : SlotId, function : &F, dimensions : &[SlotId], arguments : &[SlotId], outputs : &[SlotId])
 	{
@@ -314,7 +355,7 @@ impl<'device, 'queue> SchedulerState<'device, 'queue>
 	{
 		if self.active_command_encoder_opt.is_none()
 		{
-			let active_command_encoder = CommandEncoder{buffer_ids : BTreeSet::new(), wgpu_command_encoder_opt : None, wgpu_command_buffers : vec![]};
+			let active_command_encoder = CommandEncoder{wgpu_command_encoder_opt : None, wgpu_command_buffers : vec![]};
 			self.active_command_encoder_opt = Some(active_command_encoder);
 		}
 
@@ -338,8 +379,11 @@ impl<'device, 'queue> SchedulerState<'device, 'queue>
 		}
 	}
 
-	pub fn submit_gpu(&mut self)
+	pub fn submit(&mut self, target_place : Place)
 	{
+		// Only supported for GPU currently
+		assert_eq!(target_place, Place::Gpu);
+
 		self.local_logical_timestamp.step();
 
 		self.end_command_encoding();
@@ -348,28 +392,30 @@ impl<'device, 'queue> SchedulerState<'device, 'queue>
 		std::mem::swap(&mut active_command_encoder_opt, &mut self.active_command_encoder_opt);
 		if let Some(mut active_command_encoder) = active_command_encoder_opt
 		{
-			let command_list = CommandList{buffer_ids : active_command_encoder.buffer_ids, time_submitted : self.local_logical_timestamp};
 			self.queue.submit(active_command_encoder.wgpu_command_buffers);
 
-			for buffer_id in command_list.buffer_ids.iter()
+			/*for (slot_id, slot_opt) in self.slots.iter_mut().enumerate()
 			{
-				let buffer : &mut Buffer = self.buffers.get_mut(& buffer_id).unwrap();
-				buffer.queue_state = match buffer.queue_state
+				if let Some(slot) = slot_opt
 				{
-					QueueState::None => panic!("Buffer was not properly encoded"),
-					QueueState::Encoded => QueueState::Submitted,
-					QueueState::Submitted => QueueState::Submitted,
-					QueueState::Ready => QueueState::Ready,
-				};
-			}
-
-			self.pending_command_lists.push_back(command_list);
+					if slot.queue_place == target_place && self.local_logical_timestamp >= slot.time_observed
+					{
+						if slot.queue_state == QueueState::Encoded
+						{
+							slot.time_observed = self.local_logical_timestamp;
+							slot.queue_state = QueueState::Submitted;
+						}
+					}
+				}
+			}*/
 		}
+
+		self.transition_resource_states(target_place, QueueState::Encoded, QueueState::Submitted);
 	}
 
-	pub async fn map_local(&mut self, slot_id : SlotId, map_mode : MapMode)
+	/*pub async fn map_local(&mut self, to_slot_id : SlotId, from_slot_id : SlotId, map_mode : MapMode)
 	{
-		assert!(! self.slot_per_place_bindings[& slot_id].contains_key(& Place::Local));
+		assert!(! self.has_slot(to_slot_id));
 		let (buffer_id, start, size) = match self.slot_per_place_bindings[& slot_id][& Place::Gpu]
 		{
 			Binding::Buffer{buffer_id, start, size} => (buffer_id, start, size),
@@ -388,7 +434,6 @@ impl<'device, 'queue> SchedulerState<'device, 'queue>
 			MapMode::WriteOnly =>
 			{
 				assert_eq!(buffer.other_use_count, 0);
-				assert_eq!(buffer.queue_state, QueueState::None);
 				assert_eq!(buffer.map_count, 0);
 				wgpu::MapMode::Write
 			}
@@ -425,17 +470,11 @@ impl<'device, 'queue> SchedulerState<'device, 'queue>
 		self.slot_per_place_bindings.get_mut(& slot_id).unwrap().insert(Place::Local, binding);
 	}
 
-	fn convert_locally_mapped_to_variable<T : 'static + Copy>(&mut self, slot_id : SlotId)
+	pub fn convert_locally_mapped_to_variable<T : 'static + Copy>(&mut self, to_slot_id : SlotId, from_slot_id : SlotId)
 	{
-
 		let (size, nasty_raw_pointer) = match self.slot_per_place_bindings[& slot_id][& Place::Local]
 		{
-			Binding::ReadOnlyMappedBuffer{buffer_id, start : _, size, nasty_raw_pointer} =>
-			{
-				let buffer : &mut Buffer = self.buffers.get_mut(& buffer_id).unwrap();
-				assert_eq!(buffer.map_count, 1);
-				(size, nasty_raw_pointer)
-			}
+			Binding::ReadOnlyMappedBuffer{buffer_id : _, start : _, size, nasty_raw_pointer} => (size, nasty_raw_pointer),
 			//Binding::WriteableMappedBuffer{buffer_id : _, start : _, size, nasty_raw_pointer} => (size, nasty_raw_pointer as (*const u8)),
 			_ => panic!("Incorrect binding for slot")
 		};
@@ -444,9 +483,10 @@ impl<'device, 'queue> SchedulerState<'device, 'queue>
 		let value : T = unsafe { * std::mem::transmute::<*const u8, *const T>(nasty_raw_pointer) };
 
 		let value_box : Box<dyn Any> = Box::new(value);
-		self.unbind(&[slot_id], Place::Local);
-		self.slot_per_place_bindings.get_mut(& slot_id).unwrap().insert(Place::Local, Binding::LocalVariable{value_box});
-	}
+		self.discard(&[slot_id], Place::Local);
+		self.initialize_slot(slot_id, Place::Local, Slot{queue_state : QueueState::Ready, Binding::Variable{value_box}})
+		self.slot_per_place_bindings.get_mut(& slot_id).unwrap().insert(Place::Local, Binding::Variable{value_box});
+	}*/
 }
 
 fn main()
