d5136a523f2dcc2e8247ef0ad2fb05947b1e4fa2
Mateo Guynn <mjguynn@users.noreply.github.com>
2022-07-19 15:23:11 -0700

    Rename cfg to dominators

--- src/value/mod.rs
+++ src/value/mod.rs
@@ -3,7 +3,7 @@
 use crate::ir;
 use thiserror::Error;
 
-mod cfg;
+mod dominators;
 
 /// This error is produced when an [`ir::Dependent`](crate::ir) depends on a node which:
 ///   1. occurs after the dependent, or
