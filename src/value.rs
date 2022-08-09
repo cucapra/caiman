54ef798cb50b5bd6ee77d192caebb8333c94f4fe
Mateo Guynn <mjguynn@users.noreply.github.com>
2022-08-08 21:46:43 -0700

    Build skeleton of configurable optimizations

--- src/value.rs
+++ src/value.rs
@@ -1,6 +1,5 @@
 #![warn(warnings)]
 use crate::ir;
-use std::collections::hash_map::{Entry, HashMap};
 
 mod from_ir;
 pub use from_ir::FromIrError;
@@ -51,18 +50,62 @@ impl egg::Language for Node {
     }
 }
 
+enum SealedRewrite {
+    RunnerHook(GraphRunnerHook),
+    RewriteRule(GraphRewriteRule),
+}
+/// An opaque value graph rewrite.
+/// This may be an "abstract" rewrite which isn't actually implemented as a rewrite rule,
+/// such as redundant branch elimination.
+pub struct Rewrite(SealedRewrite);
+
 type GraphInner = egg::EGraph<Node, Analysis>;
+type GraphRunner = egg::Runner<Node, Analysis, ()>;
+type GraphRunnerHook = Box<dyn FnMut(&mut GraphRunner) -> Result<(), String> + 'static>;
+type GraphRewriteRule = egg::Rewrite<Node, Analysis>;
 type GraphId = egg::Id;
 
-pub struct Graph(GraphInner);
+pub struct Graph {
+    runner: GraphRunner,
+    rules: Vec<GraphRewriteRule>,
+}
 impl Graph {
     pub fn new(program: &ir::Program, start: ir::FuncletId) -> Result<Self, FromIrError> {
         // due to lifetime issues, we store the analysis separately while constructing it,
         // and then move it into the graph once we're done
-        let mut graph = Self(egg::EGraph::new(Analysis::new()));
+        let mut egraph = egg::EGraph::new(Analysis::new());
         let mut analysis = Analysis::new();
-        analysis.build_with_graph(&mut graph.0, program, start)?;
-        graph.0.analysis = analysis;
-        Ok(graph)
+        analysis.build_with_graph(&mut egraph, program, start)?;
+        egraph.analysis = analysis;
+        // this looks weird because egg::Runner has a weird API, don't blame me!
+        // I don't understand why there's not a constructor that takes an egraph...
+        Ok(Self {
+            runner: GraphRunner::new(Analysis::new()).with_egraph(egraph),
+            rules: Vec::new(),
+        })
+    }
+    pub fn add_rewrite(&mut self, rewrite: Rewrite) {
+        match rewrite.0 {
+            SealedRewrite::RunnerHook(hook) => {
+                self.runner.hooks.push(hook);
+            }
+            SealedRewrite::RewriteRule(rule) => {
+                self.rules.push(rule);
+            }
+        }
+    }
+    pub fn analyze(&mut self) {
+        // `run` takes self by value and returns Self, instead of taking &mut self.
+        // This
+        let mut runner = GraphRunner::new(Analysis::new());
+        // makes
+        std::mem::swap(&mut runner, &mut self.runner);
+        // me
+        let runner = runner.run(&self.rules);
+        // sad.
+        self.runner = runner;
+    }
+    pub fn elaborate_into(program: &mut ir::Program) -> ir::FuncletId {
+        todo!()
     }
 }
