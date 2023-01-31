d1566d533c7124d8c360e0b36960b8e9c7b4f47a
Checkmate <dag368@cornell.edu>
2023-01-30 21:40:06 -0800

    Disabled explication

--- src/rust_wgpu_backend/explicate_scheduling.rs
+++ src/rust_wgpu_backend/explicate_scheduling.rs
@@ -581,5 +581,6 @@ pub fn explicate_scheduling(program : &mut ir::Program)
     }
     cleanup_partials(&mut context);
     construct_pipeline(&mut context);
+    panic!("Please don't explicate yet!")
     // debug_funclets(&program);
 }
