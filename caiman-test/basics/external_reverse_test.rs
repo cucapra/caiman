7cf9ec855192bd3587ad2dbfb034ebecc877e36a
Checkmate <dag368@cornell.edu>
2023-04-12 10:38:19 -0700

    Tests fixed

--- caiman-test/basics/external_reverse_test.rs
+++ caiman-test/basics/external_reverse_test.rs
@@ -14,6 +14,6 @@ fn main() -> Result<(), String> {
     let mut join_stack_bytes = [0u8; 4096usize];
     let mut join_stack = caiman_rt::JoinStack::new(&mut join_stack_bytes);
     let instance = main::Instance::new(&mut root_state, &callbacks);
-    let result = instance.start(&mut join_stack, 1);
+    let result = instance.start(&mut join_stack);
     crate::expect_returned!(3, result.returned().map(|x| x.0))
 }
--- caiman-test/basics/trivial_test.rs
+++ caiman-test/basics/trivial_test.rs
@@ -1,6 +1,6 @@
 struct Callbacks;
 
-impl pipeline::main::CpuFunctions for Callbacks {}
+impl main::CpuFunctions for Callbacks {}
 
 #[test]
 fn main() -> Result<(), String> {
@@ -10,6 +10,6 @@ fn main() -> Result<(), String> {
     let mut join_stack_bytes = [0u8; 4096usize];
     let mut join_stack = caiman_rt::JoinStack::new(&mut join_stack_bytes);
     let instance = main::Instance::new(&mut root_state, &callbacks);
-    let result = instance.start(&mut join_stack, 1);
+    let result = instance.start(&mut join_stack);
     crate::expect_returned!(4, result.returned().map(|x| x.0))
 }
