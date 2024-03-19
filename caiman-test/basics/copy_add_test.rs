0e8fa40520ce6847664763905f718df3ae790fa4
Checkmate50 <dgeisler50@gmail.com>
2024-03-19 10:44:03 -0400

    copy test

---
+++ caiman-test/basics/copy_add_test.rs
@@ -0,0 +1,19 @@
+struct Callbacks;
+
+impl main::CpuFunctions for Callbacks {
+    fn add(&self, _: &mut dyn caiman_rt::State, x: i64, y: i64) -> main::outputs::add {
+        (x + y,)
+    }
+}
+
+#[test]
+fn main() -> Result<(), String> {
+    let callbacks = Callbacks;
+    let mut wgpu_instance = crate::util::INSTANCE.lock().unwrap();
+    let mut root_state = wgpu_instance.create_root_state();
+    let mut join_stack_bytes = [0u8; 4096usize];
+    let mut join_stack = caiman_rt::JoinStack::new(&mut join_stack_bytes);
+    let instance = main::Instance::new(&mut root_state, &callbacks);
+    let result = instance.start(&mut join_stack, 5);
+    crate::expect_returned!(13, result.returned().map(|x| x.0))
+}
