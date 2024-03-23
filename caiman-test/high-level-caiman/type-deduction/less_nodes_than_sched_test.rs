15f0decefdbe8e55ece945a30eed8386f5e24c7b
sev <stephenverderame@gmail.com>
2024-03-23 18:02:19 -0400

    Tail call optimization

---
+++ caiman-test/high-level-caiman/type-deduction/less_nodes_than_sched_test.rs
@@ -0,0 +1,29 @@
+struct Callbacks;
+
+impl main::CpuFunctions for Callbacks {
+    fn _mul_i64_i64(&self, _: &mut dyn caiman_rt::State, a: i64, b: i64) -> (i64,) {
+        (a * b,)
+    }
+    fn _lt_i64_i64(&self, _: &mut dyn caiman_rt::State, a: i64, b: i64) -> (i32,) {
+        if a < b {
+            (1,)
+        } else {
+            (0,)
+        }
+    }
+    fn _add_i64_i64(&self, _: &mut dyn caiman_rt::State, a: i64, b: i64) -> (i64,) {
+        (a + b,)
+    }
+}
+
+#[test]
+fn main() -> Result<(), String> {
+    let callbacks = Callbacks;
+    let mut wgpu_instance = crate::util::INSTANCE.lock().unwrap();
+    let mut root_state = wgpu_instance.create_root_state();
+    let mut join_stack_bytes = [0u8; 4096usize];
+    let mut join_stack = caiman_rt::JoinStack::new(&mut join_stack_bytes);
+    let instance = main::Instance::new(&mut root_state, &callbacks);
+    let result = instance.start(&mut join_stack, 10, 5);
+    crate::expect_returned!(36, result.returned().map(|x| x.0))
+}
