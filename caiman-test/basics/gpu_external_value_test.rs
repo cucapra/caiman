e3294a1f69b09031e9be4828b52451ddd0a15e1f
sev <stephenverderame@gmail.com>
2024-04-12 08:50:38 -0400

    More assembly tests

---
+++ caiman-test/basics/gpu_external_value_test.rs
@@ -0,0 +1,15 @@
+struct Callbacks;
+
+impl main::CpuFunctions for Callbacks {}
+
+#[test]
+fn main() -> Result<(), String> {
+    let mut wgpu_instance = crate::util::INSTANCE.lock().unwrap();
+    let callbacks = Callbacks;
+    let mut root_state = wgpu_instance.create_root_state();
+    let mut join_stack_bytes = [0u8; 4096usize];
+    let mut join_stack = caiman_rt::JoinStack::new(&mut join_stack_bytes);
+    let instance = main::Instance::new(&mut root_state, &callbacks);
+    let result = instance.start(&mut join_stack, 0);
+    crate::expect_returned!(1, result.returned().map(|x| x.0))
+}
