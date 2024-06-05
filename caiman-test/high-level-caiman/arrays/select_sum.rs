afc1e7f44d66d029174e78310f087f6bafa34690
Checkmate <dag368@cornell.edu>
2024-06-05 12:03:40 -0400

    test setup

---
+++ caiman-test/high-level-caiman/arrays/select_sum.rs
@@ -0,0 +1,27 @@
+struct Callbacks;
+
+impl main::CpuFunctions for Callbacks {
+    fn sum(&self, _: &mut dyn caiman_rt::State, v: i64) -> (i64,) {
+        (-v,)
+    }
+
+    fn _lt_i64_i64(&self, _: &mut dyn caiman_rt::State, a: i64, b: i64) -> (i32,) {
+        if a < b {
+            (1,)
+        } else {
+            (0,)
+        }
+    }
+}
+
+#[test]
+fn main() -> Result<(), String> {
+    let callbacks = Callbacks;
+    let mut wgpu_instance = crate::util::INSTANCE.lock().unwrap();
+    let mut root_state = wgpu_instance.create_root_state();
+    let mut join_stack_bytes = [0u8; 4096usize];
+    let mut join_stack = caiman_rt::JoinStack::new(&mut join_stack_bytes);
+    let instance = main::Instance::new(&mut root_state, &callbacks);
+    let result = instance.start(&mut join_stack, -1, 2, 3);
+    crate::expect_returned!(-2, result.returned().map(|x| x.0))
+}
